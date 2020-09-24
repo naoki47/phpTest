90f0d18afcac012ebd1e72c56b24cf037e73b58a
naoki47 <71106115+naoki47@users.noreply.github.com>
2020-09-24 19:14:50 +0900

    aaaa

--- php03.php
+++ php03.php
@@ -31,4 +31,51 @@ function max_array($arr){
 }
 echo max_array(array(3, 4, 7, 1, 9)) . "\n";
 
+//5.次のビルトイン関数の用途、使い方を自分で調べて実際に使ってみてください。 
+ //strip_tags 
+ $text="<h1>疲れたなー</h1>"; 
+ echo $text; 
+ echo strip_tags($text); 
+ echo "\n"; 
+ 
+ 
+ //array_push 
+ $arrold = array('4','2','8'); 
+ array_push($arrold,'1','5','7'); 
+ print_r ($arrold); 
+ echo "\n"; 
+ 
+ 
+ //array_merge 
+ $arrold = array('1','2','3'); 
+ $arrfix = array('4','5','6'); 
+ $result = array_merge($arrold,$arrfix); 
+ print_r($result); 
+ echo "\n"; 
+ 
+ 
+ //time, mktime 
+ echo '現在のUnixタイムスタンプ：'.time(); 
+ echo "\n"; 
+ 
+ 
+ //mktime( [ 時 [, 分 [, 秒 [, 月 [, 日 [, 年 [, int is_dst ]]]]]]] ) 
+ echo '指定のUnixタイムスタンプ：'.mktime(16, 4, 50, 9, 7, 2020); 
+ echo "\n"; 
+ 
+ 
+ //date 
+ $week = [ 
+     '日', //0 
+     '月', //1 
+     '火', //2 
+     '水', //3 
+     '木', //4 
+     '金', //5 
+     '土', //6 
+   ]; 
+ $date = date('w'); 
+ echo $week[$date] . '曜日'; 
+ ?> 
+
 ?>
